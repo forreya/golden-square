fc0f20d1496276c4157a0dd5453cfc15d04230a5
Luke Lai <lukelai.dev@gmail.com>
2023-01-20 11:54:44 +0000

    Create spec files for test

---
+++ phase-two/designing-multi-class-programs/spec/diary_integration_spec.rb
@@ -0,0 +1,11 @@
+require 'diary'
+require 'entry'
+require 'todo'
+
+describe Diary do
+    before(:each) do
+    end
+    it 'adds entry to the diary and returns all the titles as an array' do
+        
+    end
+end
