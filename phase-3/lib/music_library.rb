b046c824c90b6ee4eccc0aeab7d48d34c3a984d5
Ryan Lai <ryanlai12@gmail.com>
2023-01-23 13:34:35 +0000

    Added one mock integration test to 'MusicLibrary'

--- phase-3/lib/music_library.rb
+++ phase-3/lib/music_library.rb
@@ -1,19 +1,24 @@
 
 class MusicLibrary
   def initialize
-    # ...
+    @tracks = []
   end
 
-  def add(track) # track is an instance of Track
+  def add(track) 
+    @tracks.push(track)
+
+    # track is an instance of Track
     # Track gets added to the library
     # Returns nothing
   end
 
   def all
+    @tracks
     # Returns a list of track objects
   end
   
   def search(keyword) # keyword is a string
+    
     # Returns a list of tracks that match the keyword
   end
 end
--- phase-3/spec/music_library_integration_spec.rb
+++ phase-3/spec/music_library_integration_spec.rb
@@ -5,11 +5,21 @@ require 'track'
 describe do
   before(:each) do
     @music_library = MusicLibrary.new
-    @music_library.add(double(:fake_track))
-    @music_library.add(double(:fake_track2))
+    @fake_track_1 = double(:fake_track1, title: "The World Is Yours", artist: "SoFaygo")
+    @fake_track_2 = double(:fake_track2, title: "Cloudy Skies", artist: "Lil Skies")
+    @music_library.add(@fake_track1)
+    @music_library.add(@fake_track2)
   end
 
-  it "adds a track to the music library then returns the track" do
-    @music_library.all
+  it "returns all tracks" do
+    result = @music_library.all()
+    expect(result).to eq [@fake_track1,@fake_track2]
   end
+
+  it "searches for the keyword 'Yours' and returns the matched track" do
+    result = @music_library.search('Yours')
+    expect(result).to eq [@fake_track1]
+  end
+
+
 end
